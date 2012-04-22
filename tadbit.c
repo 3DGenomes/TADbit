f46a8ee48c5a30cc5d27c1686717d15f6f90862e
Guillaume Filion <guillaume.filion@gmail.com>
2012-04-22 22:27:27 +0200

    Implemented and tested the 'fast' heuristic.

--- tadbit.c
+++ tadbit.c
@@ -105,55 +105,278 @@ double **break_in_blocks(double *mat, int n, int i, int j, double **blocks) {
 
 }
 
-void fill_ml_matrix(double *obs, double *llik, int *dim) {
-/*
- * Fill in the upper half of 'llik' with log-likelihood values of
- * segments starting at index i and ending at index j.
- */
 
-   int n = (int) *dim;
+void remove_non_local_maxima (double *obs, double *dis, int n,
+    double **k_blk, double **d_blk, int *bkpts) {
+
+   int i, j;
+   double llik[n];
+   double ab[3][2] = {{0.0,0.0}, {0.0,0.0}, {0.0,0.0}};
+
+/*
+   * The code is not very DRY, those lines are almost duplicated
+   * in the function 'tadbit'.
+*/
+
+   // Compute the log-lik of the first segment forward...
+   for (j = 2 ; j < n ; j++) {
+      // Cut the (i,j)-blocks.
+      k_blk = break_in_blocks(obs, n, 0, j, k_blk);
+      d_blk = break_in_blocks(dis, n, 0, j, d_blk);
+
+      // Compute the likelihood and sum.
+      llik[j] =
+          ml_ab(k_blk[1], d_blk[1], ab[1], j*(j+1)/2)          +
+          ml_ab(k_blk[2], d_blk[2], ab[2], (n-j-1)*(j+1)) / 2;
+   }
+
+   // ... and the second segment backward.
+   for (j = 3 ; j < n-3 ; j++) {
+      // Cut the (i,j)-blocks.
+      k_blk = break_in_blocks(obs, n, j, n-1, k_blk);
+      d_blk = break_in_blocks(dis, n, j, n-1, d_blk);
+
+      // Compute the likelihood and sum.
+      llik[j-1] += 
+          ml_ab(k_blk[0], d_blk[0], ab[0], j*(n-j+1))       / 2  +
+          ml_ab(k_blk[1], d_blk[1], ab[1], (n-j)*(n-j+1)/2);
+   }
+
+
+   // Get local bkpts.
+   bkpts[n-1] = 1;
+   for (i = 0 ; i < n-1 ; i++) {
+      bkpts[i] = 0;
+   }
+   for (i = 3 ; i < n-1 ; i++) {
+      if (llik[i] > llik[i-1] && llik[i] > llik[i+1]) {
+         bkpts[i] = 1;
+      }
+   }
+
+
+}
+
+int *get_breakpoints(double *llik, int n, int *all_breakpoints) {
+
+   int i,j, nbreaks = 0;
+   int new_breakpoint;
+
+   double tmp;
+
+   double new_llik[n];
+   double old_llik[n];
+   // Initialize to first line of 'llik'.
+   for (j = 0 ; j < n ; j++) {
+      old_llik[j] = llik[j*n];
+   }
+
+   // Breakpoint lists. The first index is the end of the segment,
+   // the second is 1 if this position is an end (breakpoint).
+   int new_bkpt_list[n][n];
+   int old_bkpt_list[n][n];
+   // Initialize to 0.
+   for (i = 0 ; i < n ; i++) {
+      for (j = 0 ; j < n ; j++) {
+         new_bkpt_list[i][j] = 0;
+         old_bkpt_list[i][j] = 0;
+      }
+   }
+
+   double new_full_llik = old_llik[n-1];
+   double old_full_llik = -INFINITY;
+
+   while (old_full_llik < new_full_llik) {
+
+      // Update breakpoints.
+      nbreaks++;
+      for (i = 0 ; i < n ; i++) {
+         for (j = 0 ; j < n ; j++) {
+            old_bkpt_list[i][j] = new_bkpt_list[i][j];
+         }
+      }
+
+      // Cycle over end point 'j'.
+      for (j = 3 * nbreaks + 2 ; j < n ; j++) {
+         new_llik[j] = -INFINITY;
+
+         // Cycle over start point 'i'.
+         for (i = 3 * nbreaks ; i < j - 1 ; i++) {
+
+            // NAN if not a breakpoint, so next line evaluates to false.
+            tmp = old_llik[i-1] + llik[i+j*n];
+            if (tmp > new_llik[j]) {
+               new_llik[j] = tmp;
+               new_breakpoint = i-1;
+            }
+         }
+
+         // Update breakpoint list.
+         if (new_llik[j] > -INFINITY) {
+            for (i = 0 ; i < n ; i++) {
+               new_bkpt_list[j][i] = old_bkpt_list[new_breakpoint][i];
+            }
+            new_bkpt_list[j][new_breakpoint] = 1;
+         }
+
+      }
+
+      // Update full log-likelihoods.
+      old_full_llik = new_full_llik;
+      new_full_llik = new_llik[n-1];
+      for (i = 0 ; i < n ; i++) {
+         old_llik[i] = new_llik[i];
+      }
+
+   }
+   
+   for (i = 0 ; i < n ; i++) {
+      all_breakpoints[i] = old_bkpt_list[n-1][i];
+   }
+
+   return all_breakpoints;
+
+}
+
+
+int *tadbit(double *obs, int n, int fast) {
+
    int i, j, k;
 
-   double ab[3][2] = {{0.0,0.0},{0.0,0.0}, {0.0,0.0}};
+/*
+   * Allocate memory and initialize variables. The distance
+   * matrix 'dis' is the distance to the main diagonal. Every
+   * element of coordinate (i,j) is on a diagonal; the distance
+   * is the shift to the main diagonal |i-j|.
+   * 'd_blk' and 'k_blk' will hold the distance data ('d_blk')
+   * and observation data ('k_blk') when the matrices are
+   * segmented. Each segmentation defines 3 regions, which is
+   * why there are 3 such matrices. They are allocated the maximum
+   * size they can have upon segmentation for simplicity.
+   * 'ab' contains parameters 'a' and 'b' for the maximum likelihood
+   * model. Because each segmentation defines 3 regions we need
+   * 3 pairs of parameters.
+*/
 
-
-   // Build the (n,n) distance matrix.
-   double *dis = (double *) malloc(n*n * sizeof(double));
+   // double *dis = (double *) malloc(n*n * sizeof(double));
+   double dis[n*n], llik[n*n];
 
    k = 0;
    for (i = 0; i < n ; i++) {
       for (j = 0; j < n ; j++) {
-         //dis[k++] = log(1+abs(i-j));
+         llik[k] = NAN;
          dis[k++] = abs(i-j);
       }
    }
 
    // Allocate max possible size to blocks matrices.
    double **d_blk = (double **) malloc(3 * sizeof(double *));
-   d_blk[0] = (double *) malloc(n*n/4 * sizeof(double));
-   d_blk[1] = (double *) malloc(n*(n-1)/2 * sizeof(double));
-   d_blk[2] = (double *) malloc(n*n/4 * sizeof(double));
+   d_blk[0] = (double *) malloc((n+1)*(n+1)/4 * sizeof(double));
+   d_blk[1] = (double *) malloc(n*(n+1)/2 * sizeof(double));
+   d_blk[2] = (double *) malloc((n+1)*(n+1)/4 * sizeof(double));
 
    double **k_blk = (double **) malloc(3 * sizeof(double *));
-   k_blk[0] = (double *) malloc(n*n/4 * sizeof(double));
-   k_blk[1] = (double *) malloc(n*(n-1)/2 * sizeof(double));
-   k_blk[2] = (double *) malloc(n*n/4 * sizeof(double));
+   k_blk[0] = (double *) malloc((n+1)*(n+1)/4 * sizeof(double));
+   k_blk[1] = (double *) malloc(n*(n+1)/2 * sizeof(double));
+   k_blk[2] = (double *) malloc((n+1)*(n+1)/4 * sizeof(double));
+
+   // Initialize 'a' and 'b' to 0.
+   double ab[3][2] = {{0.0,0.0}, {0.0,0.0}, {0.0,0.0}};
+
+/*
+   * If 'fast' is true, a heuristic is used to speed up the
+   * algorithm. The log-likelihood is computed by inserting a
+   * single break and local maxima are used as only candidate
+   * breakpoints. Because tadbit is O(n^2) the gain is of the
+   * same order.
+*/
+   
+   int bkpts[n];
+   // By default, all breakpoints are candidates.
+   for (i = 0 ; i < n ; i++) {
+      bkpts[i] = 1;
+   }
+
+   // If 'fast', only local maxima are candidates.
+   if (fast) {
+      remove_non_local_maxima(obs, dis, n, k_blk, d_blk, bkpts);
+   }
+
+/*
+   * Compute the log-likelihood of the segments. the element
+   * (i,j) of the matrix-like array 'llik' will contain the
+   * log-likelihood of the segment starting at i and ending
+   * at j. the matrix is initialized with nan because not all
+   * elements will be computed. the lower triangular part is
+   * left out and possibily most of the elements if fast is
+   * true.
+*/
+
 
 
    for (i = 0 ; i < n-2 ; i++) {
+      // Skip if not a potential breakpoint.
+      if ((i > 0) && (bkpts[i-1] != 1)) {
+         continue;
+      }
+
       for (j = i+2 ; j < n ; j++) {
-         // Cut the (i,j)-blocks.
+         // Skip if not a potential breakpoint.
+         if (bkpts[j] != 1) {
+            continue;
+         }
+         
+         // Segment the (i,j)-blocks.
          k_blk = break_in_blocks(obs, n, i, j, k_blk);
          d_blk = break_in_blocks(dis, n, i, j, d_blk);
 
          // Get the likelihood per block and sum.
          llik[i+j*n] =
-               ml_ab(k_blk[0], d_blk[0], ab[0], i*(j-i+1))       / 2  +
-               ml_ab(k_blk[1], d_blk[1], ab[1], (j-i)*(j-i+1)/2)      +
-               ml_ab(k_blk[2], d_blk[2], ab[2], (n-j-1)*(j-i+1)) / 2;
+             ml_ab(k_blk[0], d_blk[0], ab[0], i*(j-i+1))       / 2  +
+             ml_ab(k_blk[1], d_blk[1], ab[1], (j-i)*(j-i+1)/2)      +
+             ml_ab(k_blk[2], d_blk[2], ab[2], (n-j-1)*(j-i+1)) / 2;
       }
    }
 
+
+/*
+   * The matrix 'llik' contains the log-likelihood of the
+   * segments. The breakpoints are found by the dynamic
+   * programming routine 'get_breakpoints'.
+*/
+
+   int *all_breakpoints = (int *) malloc(n * sizeof(n));
+   all_breakpoints = get_breakpoints(llik, n, all_breakpoints);
+
+   // Free allocated memory (not sure this is needed).
+   for (i = 0 ; i < 3 ; i++) {
+      free(d_blk[i]);
+      free(k_blk[i]);
+   }
+   free(d_blk);
+   free(k_blk);
+
+
+   // START DEBUG
+   for (i = 0 ; i < n ;i++) {
+      printf("%d ", all_breakpoints[i]);
+   }
+   printf("\n");
+   // END DEBUG
+
+
+   // Done!!
+   return all_breakpoints;
+
 }
 
-// TODO: write the maximization function.
+void tadbit_R_call(double *obs, int *dim, int *fast, int *R_mem) {
+
+   int *all_breakpoints = tadbit (obs, *dim, *fast);
+
+   int i;
+   for (i = 0 ; i < *dim ; i++) {
+      R_mem[i] = all_breakpoints[i];
+   }
+
+}
