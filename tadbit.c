0e748e58d75264d7117f0dcd1cbb58e8e38ef5bf
Guillaume Filion <guillaume.filion@gmail.com>
2012-05-01 22:51:33 +0200

    Wrote a fragile read function for the main.

--- tadbit.c
+++ tadbit.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
@@ -500,19 +501,91 @@ SEXP tadbit_R_call(SEXP list, SEXP fast_yn) {
 
 }
 
-int main (int argc, const char* argv[]) {
-   FILE *f = fopen(argv[1], "r");
-   char line[1024], *p, *e;
-   long v;
-   while (fgets(line, sizeof(line), f)) {
-       p = line;
-       for (p = line ; ; p = e) {
-           v = strtol(p, &e, 10);
-           if (p == e)
-               break;
-           // process v here
-           printf("%d\n", v);
-       }
+int *read_int_matrix (const char *fname, int *n_ptr) {
+/*
+   * Read integers from a file and return a line-matrix (a pointer
+   * of pointers to int). Assume that the first line is a header
+   * containing as many space characters as columns, that the number
+   * of rows is the same as the number of columns, and that each
+   * row starts with the row name.
+*/
+
+   int i, j = 0, check_row = 0, check_col, n = 0;
+   char line[65536], row_name[256], *a, *z;
+
+   int *array;
+   long u;
+
+   // Garbage collection in case of early return.
+   int *failure_exit() {
+      free(array);
+      return NULL;
    }
+
+   FILE *f = fopen(fname, "r");
+   if (f == NULL) {
+      return NULL;
+   }
+
+   // Use first line to get 'n' by counting tabs.
+   fgets(line, sizeof(line), f);
+   for (i = 0 ; i < strlen(line)-1 ; i++) {
+      if (isspace(line[i])) {
+         n++;
+      }
+   }
+
+   array = (int *) malloc (n*n * sizeof(int));
+
+   while (fgets(line, sizeof(line), f)) {
+      if (++check_row > n) {
+         printf("check_row = %d\n", check_row);
+         return failure_exit();
+      }
+
+      a = line;
+      z = NULL;
+
+      // Skip row name.
+      if (sscanf(line, "%s", row_name)) {
+         a += strlen(row_name);
+      }
+
+      check_col = 0;
+      u = strtol(a, &z, 10);
+      while (a != z) {
+         check_col++;
+         array[j++] = (int) u;
+         a = z;
+         u = strtol(a, &z, 10);
+      }
+
+      if (check_col != n) {
+         printf("n = %d, check_col = %d\n", n, check_col);
+         return failure_exit();
+      }
+
+   }
+
+   if (check_row != n) {
+      return failure_exit();
+   }
+
    fclose(f);
+   *n_ptr = n;
+   return array;
+
+}
+
+
+int main (int argc, const char* argv[]) {
+   int i, n, *some_ints = read_int_matrix(argv[1], &n);
+   if (some_ints != NULL) {
+      double *counts = (double *) malloc(n*n * sizeof(double));
+      for (i = 0 ; i < n*n ; i++) {
+         counts[i] = (double) some_ints[i];
+      }
+      double **obs = &counts;
+      int *breakpoints = tadbit(obs, n, 1, 1);
+   }
 }
