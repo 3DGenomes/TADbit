0489c315bc1affe1c1b34d7540ef7db672f2fb92
Francois Serra <fransua@smooth.local>
2013-01-26 18:17:28 +0100

    more accurate and faster alignment of Hi-C matrices. Using Spearman Rho value as score.

--- pytadbit/tad_clustering/tad_cmo.py
+++ pytadbit/tad_clustering/tad_cmo.py
@@ -8,32 +8,32 @@ import numpy as np
 from itertools import product
 
 def core_nw(p_scores, penalty, l_p1, l_p2):
-    scores = virgin_score(penalty, l_p1, l_p2)
-    for i in xrange(1, l_p1):
-        for j in xrange(1, l_p2):
-            match  = p_scores[i][j] + scores[i-1][j-1]
+    scores = virgin_score(penalty, l_p1 + 1, l_p2 + 1)
+    for i in xrange(1, l_p1 + 1):
+        for j in xrange(1, l_p2 + 1):
+            match  = p_scores[i - 1][j - 1] + scores[i-1][j-1]
             insert = scores[i-1][j] + penalty
             delete = scores[i][j-1] + penalty
             scores[i][j] = max((match, insert, delete))
     align1 = []
     align2 = []
-    i = l_p1 - 1
-    j = l_p2 - 1
-    while i > 0 and j > 0:
+    i = l_p1 
+    j = l_p2 
+    while i and j:
         score      = scores[i][j]
-        value = scores[i-1][j-1] + p_scores[i][j]
+        value = scores[i-1][j-1] + p_scores[i - 1][j - 1]
         if equal(score, value):
-            align1.insert(0, i)
-            align2.insert(0, j)
+            align1.insert(0, i - 1)
+            align2.insert(0, j - 1)
             i -= 1
             j -= 1
         elif equal(score, scores[i-1][j] + penalty):
-            align1.insert(0, i)
+            align1.insert(0, i - 1)
             align2.insert(0, '-')
             i -= 1
         elif equal(score, scores[i][j-1] + penalty):
             align1.insert(0, '-')
-            align2.insert(0, j)
+            align2.insert(0, j - 1)
             j -= 1
         else:
             for x in scores:
@@ -41,8 +41,7 @@ def core_nw(p_scores, penalty, l_p1, l_p2):
             raise Exception('Something  is failling and it is my fault...')
     return align1, align2
 
-
-def optimal_cmo(p1, p2, num_v=None):
+def optimal_cmo(tad1, tad2, num_v=None):
     """
     :argument p1: first matrix to align
     :argument p2: second matrix to align
@@ -55,11 +54,13 @@ def optimal_cmo(p1, p2, num_v=None):
           their absolute values
     """
 
-    l_p1 = len(p1)
-    l_p2 = len(p2)
-    val1, vec1 = np.linalg.eig(p1)
-    val2, vec2 = np.linalg.eig(p2)
+    l_p1 = len(tad1)
+    l_p2 = len(tad2)
+    val1, vec1 = np.linalg.eig(tad1)
+    val2, vec2 = np.linalg.eig(tad2)
     #
+    val1 = np.array([np.sqrt(abs(v)) for v in val1])
+    val2 = np.array([np.sqrt(abs(v)) for v in val2])
     idx = val1.argsort()[::-1]
     val1 = val1[idx]
     vec1 = vec1[idx]
@@ -67,28 +68,26 @@ def optimal_cmo(p1, p2, num_v=None):
     val2 = val2[idx]
     vec2 = vec2[idx]
     #
-    val1 = [np.sqrt(abs(v)) for v in val1]
-    val2 = [np.sqrt(abs(v)) for v in val2]
-    vec1  = np.array([val1[i] * vec1[:,i] for i in xrange(num_v)]).transpose()
-    vec2p = np.array([val2[i] * vec2[:,i] for i in xrange(num_v)]).transpose()
+    vec1 = np.array([val1[i] * vec1[:,i] for i in xrange(num_v)]).transpose()
+    vec2 = np.array([val2[i] * vec2[:,i] for i in xrange(num_v)]).transpose()
     best_sc = 0
     best_alis = []
-    p_scores = [[0 for _ in xrange(l_p2)] for _ in xrange(l_p1)]
-    for factors in product([1,-1], repeat=num_v):
-        vec1p = factors * vec1
-        for i in xrange(l_p1):
-            for j in xrange(l_p2):
-                p_scores[i][j] = sum([vec1p[:,k][i] * vec2p[:,k][j] \
-                                      for k in xrange(num_v)])
-        penalty = min([min(s) for s in p_scores] + [0.0])
-        align1, align2 = core_nw(p_scores, penalty, l_p1, l_p2)
-        score = get_score(align1, align2, p1, p2)
-        if score > best_sc:
-            best_sc = score
-            best_alis = [align1, align2]
-
-    align1, align2 = best_alis
-    #print_score(align1, align2, p1, p2)
+    for num in xrange(1, num_v + 1):
+        for factors in product([1,-1], repeat=num):
+            vec1p = factors * vec1[:,:num]
+            vec2p = vec2[:,:num]
+            p_scores = prescoring(vec1p, vec2p, num, l_p1, l_p2)
+            penalty = min([min(s) for s in p_scores] + [0.0])
+            align1, align2 = core_nw(p_scores, penalty, l_p1, l_p2)
+            score = get_score(align1, align2, tad1, tad2)
+            if score > best_sc:
+                best_sc = score
+                best_alis = [align1, align2]
+    try:
+        align1, align2 = best_alis
+    except ValueError:
+        pass
+    print_score(align1, align2, tad1, tad2)
     print '\n Alignment (score = {}):'.format(best_sc)
     print 'TADS 1: '+'|'.join(['%4s' % (str(int(x)) \
                                         if x!='-' else '-'*3) for x in align1])
@@ -108,7 +107,29 @@ def equal(a, b, cut_off=1e-9):
     return abs(a-b) < cut_off
 
 
-def get_score(align1, align2, p1, p2):
+def prescoring(vec1p, vec2p, num, l_p1, l_p2):
+    """
+    yes... this is the bottle neck, half of the time spent here
+    """
+    return [[sum(vec1p[i] * vec2p[j]) for j in xrange(l_p2)] for i in xrange(l_p1)]
+
+
+def get_score(align1, align2, tad1, tad2):
+    """
+    using Spearman Rho value
+    """
+    map1 = []
+    map2 = []
+    for i, j in zip(align1, align2):
+        if j != '-' and i != '-':
+            map1.append(i)
+            map2.append(j)
+    pp1 = [tad1[i][j] for i in map1 for j in map1]
+    pp2 = [tad2[i][j] for i in map2 for j in map2]
+    return np.corrcoef(pp1, pp2, rowvar=0)[1, 0]
+
+
+def get_OLD_score(align1, align2, p1, p2):
     map1 = []
     map2 = []
     for i, j in zip(align1, align2):
@@ -124,6 +145,12 @@ def get_score(align1, align2, p1, p2):
                for i in xrange(com) for j in xrange(i + 1, com)])
     return float(2 * cmo)/(cm1+cm2)
 
+
+def prescoring(vec1p, vec2p, num, l_p1, l_p2):
+    """
+    yes... this is the bottle neck, half of the time spent here
+    """
+    return [[sum(vec1p[i] * vec2p[j]) for j in xrange(l_p2)] for i in xrange(l_p1)]
 
 
 def main():
