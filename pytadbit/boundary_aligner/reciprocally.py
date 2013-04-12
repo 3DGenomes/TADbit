a6a5586b3aadff7d42ef2bc21053296d1c5a01cf
garamonfok <francois@barrabin.org>
2013-04-12 17:40:20 +0200

    functional reciprocal. Not linked neither tested yet

--- pytadbit/boundary_aligner/reciprocally.py
+++ pytadbit/boundary_aligner/reciprocally.py
@@ -17,41 +17,63 @@ def find_closest(num, tads1):
     return closest
 
 
-def find_closest_reciprocal(num, tads1, tads2, start=0):
+def find_closest_reciprocal(num, tads1, tads2, penalty, start=0):
     """
     function to check the needleman_wunsch algorithm.
     """
-    closest = 0
+    closest = None
     diff = inf = float('inf')
+    gap = 0
     for n in tads2:
         if n < start: continue
         if abs(n - num) < diff:
             other_num = find_closest(n, tads1)
-            print n, num, other_num
             if num == other_num:
                 diff = abs(n - num)
+                if closest > -1:
+                    gap += 1
                 closest = n
-            else:
-                print 'gap', num, other_num
         elif diff != inf:
             break
     if diff == inf:
-        return '---', None
+        return '-', None, penalty
     #return None, None
-    return closest, diff
+    return closest, gap, diff
 
 
-def reciprocal(tads1, tads2):
+def reciprocal(tads1, tads2, penalty=None, verbose=False):
+    """
+
+    :argument None penalty: if 
+    
+    tads1 = [1, 5, 6, 9, 18, 22, 33, 34, 36]
+    tads2 = [0, 1, 7, 9, 17, 21, 26, 29, 35]
+    """
+    if not penalty:
+        # set penalty to the average length of a TAD
+        penalty = float(reduce(lambda x, y: abs(x - y), tads1))/len(tads1)
+        penalty += float(reduce(lambda x, y: abs(x - y), tads2))/len(tads2)
     start = 0
     diffs = []
     align1 = []
     align2 = []
+    i = 0
     for t in tads1:
-        closest, diff = find_closest_reciprocal(t, tads1, tads2, start=start)
-        if closest != '---':
+        closest, gap, diff = find_closest_reciprocal(t, tads1, tads2,
+                                                     penalty, start=start)
+        if closest != '-':
             start=closest
-        print t, closest
         diffs.append(diff)
+        while gap:
+            align1.append('-')
+            align2.append(tads2[i])
+            i += 1
+            gap -= 1
         align1.append(t)
         align2.append(closest)
+        i += 1
+    if verbose:
+        print 'TAD1: ' + '|'.join(['{:>3}'.format(a) for a in align1])
+        print 'TAD2: ' + '|'.join(['{:>3}'.format(a) for a in align2])
+    return align1, align2, float(sum(diffs))/len(align1)
         
