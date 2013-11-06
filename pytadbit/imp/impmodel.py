e31e43db2d75df41144aaf7b76c6c3ffe56b342f
fransua <serra.francois@gmail.com>
2013-11-06 11:04:26 +0100

    corrected conversion, we work with angstrom...

--- pytadbit/imp/impmodel.py
+++ pytadbit/imp/impmodel.py
@@ -589,10 +589,11 @@ class IMPmodel(dict):
             acc_parts.append((acc, ina))
 
         # some stats
-        dot_area = 4 * pi * (float(radius) / 1000)**2 / nump
+        # convert angstrom to micrometers
+        dot_area = 4 * pi * (float(radius) / 10000)**2 / nump
         area = ((len(subpoints) - impossibles) * dot_area)
-        total = (self.contour() / 1000 * 2 * pi * float(radius) / 1000 + 4 * pi
-                 * (float(radius) / 1000)**2)
+        total = (self.contour() / 10000 * 2 * pi * float(radius) / 10000 + 4 * pi
+                 * (float(radius) / 10000)**2)
         if verbose:
             print (' Accessible surface: %s micrometers^2' +
                    '(%s accessible times %s micrometers)') % (
