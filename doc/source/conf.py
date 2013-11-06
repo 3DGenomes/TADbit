8023ce42b007cedc15950d7f8bdc654443cc156c
fransua <serra.francois@gmail.com>
2013-11-06 22:25:47 +0100

    test doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,8 @@ sys.path.insert(0, os.path.abspath('../../pytadbit/'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
               'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.mathjax',
-              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode',
+              "sphinxtogithub" ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
