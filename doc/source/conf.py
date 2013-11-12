9b2f764120a8482c090fbb8298879e0baf033a3f
fransua <serra.francois@gmail.com>
2013-11-12 17:19:19 +0100

    added logo!

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -113,12 +113,13 @@ html_theme = 'nature'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'pictures/Tadbit logo_little.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'pictures/Tadbit icon.png'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
