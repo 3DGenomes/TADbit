a7b4218772ac35d6f555ea9b4a8d9495bbec9652
fransua <serra.francois@gmail.com>
2013-11-12 17:23:09 +0100

    added icon!

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -118,7 +118,7 @@ html_logo = 'pictures/Tadbit logo_little.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'pictures/Tadbit icon.png'
+html_favicon = 'pictures/Tadbit icon.ico'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
