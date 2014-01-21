ac18f7faf8beb16529f3115fa4162c417072284c
fransua <serra.francois@gmail.com>
2014-01-21 21:25:34 +0100

    added try-catch on init import

--- _pytadbit/__init__.py
+++ _pytadbit/__init__.py
@@ -6,7 +6,10 @@ from pytadbit.experiment import Experiment
 from pytadbit.chromosome import load_chromosome
 from pytadbit.imp.structuralmodels import StructuralModels
 from pytadbit.imp.structuralmodels import load_structuralmodels
-from pytadbit.imp.impoptimizer import IMPoptimizer
 from pytadbit.imp.impmodel import load_impmodel_from_cmm
 from pytadbit.imp.impmodel import load_impmodel_from_xyz
 from pytadbit.imp.impmodel import IMPmodel
+try:
+    from pytadbit.imp.impoptimizer import IMPoptimizer
+except ImportError:
+    warn('IMP not found, check PYTHONPATH\n')
