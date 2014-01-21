3ddebfcfc916639a61e24b044b5ac525909bbca7
fransua <serra.francois@gmail.com>
2014-01-21 18:29:26 +0100

    added imports in main __init__. Solves #32

--- _pytadbit/__init__.py
+++ _pytadbit/__init__.py
@@ -4,3 +4,9 @@ from pytadbit.tadbit import tadbit, batch_tadbit
 from pytadbit.chromosome import Chromosome
 from pytadbit.experiment import Experiment
 from pytadbit.chromosome import load_chromosome
+from pytadbit.imp.structuralmodels import StructuralModels
+from pytadbit.imp.structuralmodels import load_structuralmodels
+from pytadbit.imp.impoptimizer import IMPoptimizer
+from pytadbit.imp.impmodel import load_impmodel_from_cmm
+from pytadbit.imp.impmodel import load_impmodel_from_xyz
+from pytadbit.imp.impmodel import IMPmodel
