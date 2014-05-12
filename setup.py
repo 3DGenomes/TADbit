3e7c338b0056594d31ca45a0b2513735b65b90d6
fransua <serra.francois@gmail.com>
2014-05-12 20:11:41 +0200

    updated doc and readme

--- setup.py
+++ setup.py
@@ -142,6 +142,7 @@ def main():
                                 stderr=PIPE).communicate()
         plus = 'nothing to commit' not in git_status
         git_version  = git_revision.split('-')[0]
+        print 'git_revision', git_revision, 'err', err
         git_revision = str(int(git_revision.split('-')[1]) + plus)
     except OSError:
         git_revision = revision
