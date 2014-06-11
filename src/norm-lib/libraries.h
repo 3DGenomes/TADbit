1ad151a0336460d3f1076b1826f34c837109d07e
fransua <serra.francois@gmail.com>
2014-06-11 20:34:48 +0200

    #61 added original script from Ivan Junier

---
+++ src/norm-lib/libraries.h
@@ -0,0 +1,28 @@
+/*
+ *  Created by Ivan Junier
+ *  
+ */
+
+#ifndef _LIBRARIES_
+#define _LIBRARIES_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pstream.h>
+#include <cfloat>
+#include <climits>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <boost/regex.hpp>
+#include <boost/regex/v4/regex.hpp>
+#endif
