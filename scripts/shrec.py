88782cb15cf241b91d7d510d3b7a9c04298a6f29
fransua <serra.francois@gmail.com>
2014-10-03 11:51:55 +0200

    updated shrec

--- scripts/shrec.py
+++ scripts/shrec.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python
+
 """
 25 sept. 2014
 
@@ -11,7 +13,7 @@ Nature Methods, 4, 10-13. doi:10.1038/nmeth.3104
 
 from argparse import ArgumentParser
 from mpl_toolkits.mplot3d import Axes3D
-from numpy import linalg, array, log2, copy, minimum, newaxis, median
+from numpy import linalg, array, log2, copy, minimum, newaxis, percentile
 from matplotlib import pyplot as plt
 
 
@@ -70,11 +72,16 @@ def main():
     cmm = opts.cmm
     strict_julien = opts.strict
 
-    print '- Getting data'
-    data = [[float(v) for v in l.split()] for l in open(fnam)]
+    if opts.verbose:
+        print '- Getting data'
+    try:
+        data = [[float(v) for v in l.split()] for l in open(fnam)]
+    except ValueError:
+        data = [[float(v) for v in l.split(',')] for l in open(fnam)]
 
     N = len(data)
-    print '- Make the data symmetric'
+    if opts.verbose:
+        print '- Make the data symmetric'
     total = 0
     for i in xrange(N):
         for j in xrange(i + 1, N):
@@ -82,64 +89,96 @@ def main():
             data[j][i] = data[i][j]
             total += data[j][i]
 
-    print '- Replace zeros'
+    if opts.verbose:
+        print '- Replace zeros'
     if strict_julien:
-        minv = total /N**2
+        minv = total / N**2
     else:
         minv = min([data[i][j] for j in xrange(N)
                     for i in xrange(N) if data[i][j]]) /2
-    data = array([array([1./(data[i][j] or minv) for j in xrange(N)])
-                  for i in xrange(N)])
-
+    if not opts.dist:
+        data = array([array([1./(data[i][j] or minv) for j in xrange(N)])
+                      for i in xrange(N)])
+    else:
+        data = array([array([data[i][j] for j in xrange(N)])
+                      for i in xrange(N)])
+    
     ############################################################################
     ### Here it starts
     ############################################################################
-    print '- Finding shortest path'
-    if opts.fw == 'numpy':
-        D = floyd_warshall_numpy(adj(data))
-    else:
-        import pyximport
-        pyximport.install(reload_support=True)
-        import floyd_warshall
-        if opts.fw == 'cythonP':
-            D = floyd_warshall.floyd_warshall_parallelized(adj(data))
-        elif opts.fw == 'cython':
-            D = floyd_warshall.floyd_warshall_single_core(adj(data))
+    if not opts.dist:
+        if opts.verbose:
+            print '- Finding shortest path'
+        if opts.fw == 'numpy':
+            D = floyd_warshall_numpy(adj(data))
         else:
-            raise NotImplementedError('choose between cython cythonP or numpy')
-
-    # scale distances
-    meand = median([D[i][i+1] for i in xrange(N-1)])
-    factor = 5 * float(scale) / meand
+            import pyximport
+            pyximport.install(reload_support=True)
+            import floyd_warshall
+            if opts.fw == 'cythonP':
+                D = floyd_warshall.floyd_warshall_parallelized(adj(data))
+            elif opts.fw == 'cython':
+                D = floyd_warshall.floyd_warshall_single_core(adj(data))
+            else:
+                raise NotImplementedError('choose between cython cythonP or numpy')
+    else:
+        D = data
 
     # get the square of D for performence
-    D2 = [[(factor*D[i][j])**2 for j in xrange(N)] for i in xrange(N)]
+    D2 = [[(D[i][j])**2 for j in xrange(N)] for i in xrange(N)]
 
-    print '- Distance to barycenter'
+    if opts.verbose:
+        print '- Distance to barycenter'
     uptri = sum([sum(D2[j][j + 1:N]) for j in xrange(N)]) / (N**2)
     dO2 = [sum(D2[i]) / N - uptri for i in xrange(N)]
 
     # metric matrix
-    print '- Calculating metric matrix'
+    if opts.verbose:
+        print '- Calculating metric matrix'
     M = [[(dO2[i] + dO2[j] - D2[i][j]) / 2 for j in xrange(N)]
          for i in xrange(N)]
 
     # Eigen vectors and values
-    print '- Getting eigenvectors and eigenvalues'
+    if opts.verbose:
+        print '- Getting eigenvectors and eigenvalues'
     Eval, Evect = linalg.eigh(array(M))
     # sort eigen vectors
-    idx = (-Eval).argsort()
+    idx = (-abs(Eval)).argsort()
     Eval = Eval[idx]
     Evect = Evect[:, idx]
+    # print '-'*80
+    # print ' '.join(['%.1f'%e for e in Eval])
+    # print '-'*80
+    # print (sum(Eval[:3]), sum([abs(e) for e in Eval]),
+    #        sum(Eval[:3]) / sum([abs(e) for e in Eval]))
+    if opts.verbose:
+        print ('    contribution of the 3 first eigenvalues: %.1f%%'  %
+               (sum(Eval[:3]) / sum([abs(e) for e in Eval]) * 100))
 
     # Coordinates
-    print '- Estimating 3D coordinates'
+    if opts.verbose:
+        print '- Estimating 3D coordinates'
     x = [Evect[i][0] * Eval[0]**.5 for i in xrange(N)]
     y = [Evect[i][1] * Eval[1]**.5 for i in xrange(N)]
     z = [Evect[i][2] * Eval[2]**.5 for i in xrange(N)]
     ############################################################################
     ### And that's it...
     ############################################################################
+    if opts.verbose:
+        print '- Generating outfiles'
+    if scale:
+        if opts.verbose:
+            print '  * scaling'
+        # scale distances
+        distances = []
+        for i in xrange(N - 1):
+            distances.append(distance((x[i], y[i], z[i]),
+                                      (x[i + 1], y[i + 1], z[i + 1])))
+        meand = percentile(distances, 25)
+        factor = float(scale) / meand
+        x = [i * factor for i in x]
+        y = [i * factor for i in y]
+        z = [i * factor for i in z]
     if show:
         plt.figure(figsize=(18, 7))
         plt.subplot(121)
@@ -151,9 +190,22 @@ def main():
         plot_3d_model(x, y, z, axe=axe)
         plt.show()
     if cmm:
-        write_cmm(x, y, z, fnam.split('.')[0] + '.cmm', radius=30)
-        write_xyz(x, y, z, fnam.split('.')[0] + '.xyz')
-
+        if opts.verbose:
+            print '  * writing CMM and XYZ files'
+        write_cmm(x, y, z, fnam + '.cmm', radius=30)
+        write_xyz(x, y, z, fnam + '.xyz')
+    if opts.mtx:
+        if opts.verbose:
+            print '  * writing distance matrix'
+        out = open(fnam + '.tsv', 'w')
+        for i in xrange(N):
+            out.write('\t'.join(['%.4f' % distance((x[i], y[i], z[i]),
+                                                   (x[j], y[j], z[j]))
+                                 for j in xrange(N)]))
+            out.write('\n')
+        out.close()
+    if opts.verbose:
+        print '\nDone\n\n'
 
 
 def get_options():
@@ -162,17 +214,25 @@ def get_options():
     parser.add_argument('-i', '--infile', dest='fnam',
                         default=False, help='matrix file.')
     parser.add_argument('--cmm', dest='cmm', action='store_true',
-                        default=False, help='generate cmm and xyz files.')
+                        default=False, help='generate cmm and xyz files')
+    parser.add_argument('--mtx', dest='mtx', action='store_true',
+                        default=False, help='generate distance matrix file')
     parser.add_argument('--show', dest='show', action='store_true',
-                        default=False, help='display matplotlib 3D plot.')
+                        default=False, help='display matplotlib 3D plot')
+    parser.add_argument('--dist', dest='dist', action='store_true',
+                        default=False, help='''input is already a distance
+                        matrix''')
+    parser.add_argument('--verbose', dest='verbose', action='store_true',
+                        default=False, help='''print process info''')
     parser.add_argument('--strict', dest='strict', action='store_true',
                         default=False, help='''Replace zeros by sum of the
                         matrix divided by the square of the number of rows
                         (recommended for binary matrices). By default it uses
-                        half the minimum value.''')
+                        half the minimum value''')
     parser.add_argument('--scale', dest='scale',
-                        default=200, help='''[%(default)s] Average distance (nm)
-                        beetween two particles.''')
+                        default=0, help='''[%(default)s] Average distance (nm)
+                        beetween two particles; by default no scalling is
+                        applied''')
     parser.add_argument('--fw', dest='fw', default='numpy',
                         help='''[%(default)s] implementation to
                         search shortest path using Floyd-Warshall can be one of
@@ -183,6 +243,20 @@ def get_options():
     if not opts.fnam:
         exit(parser.print_help())
     return opts
+
+
+def distance(part1, part2):
+    """
+    Calculates the distance between two particles.
+
+    :param part1: coordinate in list format (x, y, z)
+    :param part2: coordinate in list format (x, y, z)
+
+    :returns: distance between two points in space
+    """
+    return ((part1[0] - part2[0])**2 +
+            (part1[1] - part2[1])**2 +
+            (part1[2] - part2[2])**2)**.5
 
 
 def color_residues(x):
@@ -309,7 +383,7 @@ def write_cmm(x, y, z, path, radius=30, **kwargs):
                        x[i], y[i], z[i],
                        color[i][0], color[i][1], color[i][2], i + 1)
     form = ('<link id1=\"%s\" id2=\"%s\" r=\"1\" ' +
-            'g=\"1\" b=\"1\" radius=\"' + str(10) +
+            'g=\"1\" b=\"1\" radius=\"' + str(radius / 3) +
             # str(self['radius']/2) +
             '\"/>\n')
     for i in xrange(1, len(x)):
