05fcd4c87d9f2419fd41e9060002cd6fa1871d15
fransua <serra.francois@gmail.com>
2016-04-13 12:18:58 +0200

    start deprecating full_mapper script

--- _pytadbit/mapping/mapper.py
+++ _pytadbit/mapping/mapper.py
@@ -1,4 +1,5 @@
+from pytadbit.mapping.full_mapper import *
 
 def get_intersection(fname1, fname2, out_path, verbose=False):
     raise DeprecationWarning('WARNING: deprecated, import get_intersection directly like this: '
-                             '\n  from pytadbit.mapper import get_intersection')
+                             '\n  from pytadbit.mapping import get_intersection')
